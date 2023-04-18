a87044b9e99e92db5ef7b9f41e3e93759ae4e2c0
Phuoc Nguyen <nphuoc0402@gmail.com>
2023-04-18 16:01:30 +0900

    fix id Room 404

--- IOSDemo/ListViewModel/RoomOrderController.swift
+++ IOSDemo/ListViewModel/RoomOrderController.swift
@@ -11,7 +11,7 @@ class RoomOrderController: ObservableObject {
     
     let viewContext = PersistenceController.shared.container.viewContext
     init() {
-        //deleteRoomOrder()
+//        deleteRoomOrder()
     }
 
     func getRoomOrderByUser(userId: String) -> [RoomOrder] {
